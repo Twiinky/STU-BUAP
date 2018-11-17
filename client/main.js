69062a6104aa0edc3d1714cab4f326fbda458abe
Josue <eusoj16@gmail.com>
2018-11-16 20:18:21 -0600

    commit6

--- client/main.js
+++ client/main.js
@@ -1,15 +1,26 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Boletos } from '../lib/collections.js';
+import { Vendedor } from '../lib/collections.js';
 
 import './main.html';
 
 Router.configure({
-	layoutTemplate: 'principal'
+	layoutTemplate: 'principal',
+	//layoutTemplate: 'principalVendedor'
 });
 Router.route('/registro');
 Router.route('/ingresar');
+Router.route('/navegacion');
+
+Router.route('/registroVendedor');
+Router.route('/ingresarVendedor');
+Router.route('/navegacionVendedor');
+
 Router.route('/SesionUsuario');
+Router.route('/SesionVendedor');
+Router.route('/SesionChofer');
+
 Router.route('/SesionError');
 Router.route('/',{
 	name:'inicio',
@@ -37,7 +48,7 @@ Template.registro.events({
 		Boletos.insert({
        		matricula: matricula,
        		correo: email,
-
+       		boletos: 0
    		});
      
       
@@ -46,7 +57,42 @@ Template.registro.events({
 	}
 });
 
+Template.registroVendedor.events({
+	'submit form': function(event){
+		event.preventDefault();
+		var email = $('[name=email]').val();
+		var password = $('[name=password]').val();
+		var matricula = $('[name=matricula]').val();
+		token=email;
+		token2=matricula;
+		Accounts.createUser({
+            email: email,
+            password: password,
+        
+        });
+		
+    
+		Vendedor.insert({
+       		matricula: matricula,
+       		correo: email
+   		});
+     
+      
+      
+		Router.go('SesionVendedor');
+	}
+});
+
+
 Template.navegacion.events({
+	'click .logout': function(event){
+		event.preventDefault();
+		Meteor.logout();
+		Router.go('inicio');
+	}
+});
+
+Template.navegacionVendedor.events({
 	'click .logout': function(event){
 		event.preventDefault();
 		Meteor.logout();
@@ -72,6 +118,24 @@ Template.ingresar.events({
 	}
 });
 
+
+Template.ingresarVendedor.events({
+	'submit form': function(event){
+		event.preventDefault();
+		var email=$('[name=email]').val();
+		var password=$('[name=password]').val();
+		Meteor.loginWithPassword(email, password, function(error){
+    if(error){
+        console.log(error.reason);
+        Router.go('SesionError');
+    } else {
+    	token=email;
+        Router.go('SesionVendedor');
+    }
+});
+		
+	}
+});
 Template.codigo.onRendered(function () {
     $('#qrcode').qrcode({
       size: 250,
@@ -87,16 +151,32 @@ Template.codigo.onRendered(function () {
     return false;
   });
 
+Template.SesionUsuario.helpers({
+	'boleto' : function(){
+		var bol=Boletos.findOne({correo:token});
+		var boletos=bol.boletos;
+		return(boletos);
+	}
+});
 
-
+Template.SesionVendedor.helpers({
+	'vendedor' : function(){
+		var ven=Vendedor.findOne({correo:token});
+		var nombreVendedor=ven.matricula;
+		return(nombreVendedor);
+	}
+});
 
 Template.codigo.helpers({
 	'matricul': function(){
 
 	var matricula_codigo=Boletos.findOne({correo:token});
 	var newmatricula_codigo=matricula_codigo.matricula;
+	var nombre=matricula_codigo.correo;
+	//var boletos=matricula_codigo.boletos;
 
-	console.log(newmatricula_codigo);
+
+	
 	token2=newmatricula_codigo;
 	//console.log(typeof newmatricula_codigo);
 	return(newmatricula_codigo);
